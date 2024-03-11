e04d07eecf44dc3ff7aa7c79b3a5d64645f081a7
danodoms <danodoms@gmail.com>
2024-03-11 21:01:11 +0800

    added production packages

--- app.js
+++ app.js
@@ -1,36 +1,63 @@
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const catalogRouter = require("./routes/catalog"); //Import routes for "catalog" area of site
+
+// COMPRESSION
+const compression = require("compression");
+
+// HELMET
+const helmet = require("helmet");
+
+// EXPRESS RATE LIMIT
+const RateLimit = require("express-rate-limit");
+
 const createError = require("http-errors");
+
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
-const catalogRouter = require("./routes/catalog"); //Import routes for "catalog" area of site
-
-
 const app = express();
 
 const wiki = require("./routes/wiki.js");
 
-
-
 // Set up mongoose connection
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const mongoDB =
+
+const dev_db_url =
   "mongodb+srv://admin:admin@cluster0.wvucgbd.mongodb.net/local_library?retryWrites=true&w=majority";
+
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 
 main().catch((err) => console.log(err));
 async function main() {
   await mongoose.connect(mongoDB);
 }
 
-
-
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
+
+app.use(compression()); // Compress all routes
+
+// HELMET SETUP
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  })
+);
+
+// EXPRESS RATE LIMITER SETUP
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 30,
+});
+
+app.use(limiter);
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -42,7 +69,6 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/wiki", wiki);
 app.use("/catalog", catalogRouter); // Add catalog routes to middleware chain.
-
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
